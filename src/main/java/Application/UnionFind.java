d5f064bcde3f520953b1c54bc2f5ee2dd7873ecb
LIP17 <lipei.work@gmail.com>
2017-09-25 22:27:00 -0400

    chapter1: finished with all unionfind case

--- src/main/java/Application/UnionFind.java
+++ src/main/java/Application/UnionFind.java
@@ -69,18 +69,98 @@ class UnionFindWithQuickFind implements UnionFind {
 }
 
 
-class UnionFindWithQuickUnion {
+class UnionFindWithQuickUnion implements UnionFind{
 
+    // index is id of obj, value is its direct ancestor (like tree)
+    private int[] ids;
+    private int count;
 
+    public UnionFindWithQuickUnion(int N) {
+        ids = new int[N];
+        for(int i = 0; i < N; i++) ids[i] = i;
+        count = N;
+    }
+
+    @Override
+    public int count() { return count; }
+
+    @Override
+    public boolean connected(int p, int q) { return find(p) == find(q); }
+
+    // find the ancestor obj id of p
+    @Override
+    public int find(int p) {
+        while (ids[p] != p) p = ids[p];
+        return p;
+    }
+
+    // when input like (1,2) (1,3) (1,4) this is still O(N^2)
+    @Override
+    public void union(int p, int q) {
+        int pRootId = find(p);
+        int qRootId = find(q);
+
+        if(pRootId == qRootId) return;
+
+        // connect two components to same ancestor.
+        ids[pRootId] = qRootId;
+
+        count--;
+    }
 }
 
+/**
+ * Always join the smaller tree to larger tree avoid the performance issue
+ * when accessing pattern like (0,1), (0,2)...
+ * */
+class UnionFindWithWeightedQuickUnion implements UnionFind {
 
+    private int[] ids;
+    private int[] depth;
 
+    // number of components
+    private int count;
 
+    public UnionFindWithWeightedQuickUnion(int N) {
+        this.count = N;
 
+        ids = new int[N];
+        for(int i = 0; i < N; i++) ids[i] = i;
 
+        depth = new int[N];
+        for (int i = 0; i < N; i++) depth[i] = 1;
+    }
 
+    @Override
+    public int count() { return this.count; }
 
+    @Override
+    public boolean connected(int p, int q) { return find(p) == find(q); }
 
+    @Override
+    public void union(int p, int q) {
+        int pRootId = find(p);
+        int qRootId = find(q);
 
+        if(pRootId == qRootId) return;
 
+        int pDepth = depth[pRootId];
+        int qDepth = depth[qRootId];
+
+        if(pDepth > qDepth) {
+            ids[qRootId] = pRootId;
+            depth[pRootId] += depth[qRootId];
+        } else {
+            ids[pRootId] = qRootId;
+            depth[qRootId] += depth[pRootId];
+        }
+
+        count--;
+    }
+
+    @Override
+    public int find(int p) {
+        while (ids[p] != p) p = ids[p];
+        return p;
+    }
+}
