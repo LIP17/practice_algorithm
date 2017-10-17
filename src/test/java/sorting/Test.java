9376464f9110ce7451127aa39a22c17426f15488
LIP17 <lipei.work@gmail.com>
2017-10-17 01:13:04 -0400

    chapter2: heap, priority queue and heapsort, chapter finished!

--- src/test/java/sorting/Test.java
+++ src/test/java/sorting/Test.java
@@ -1,18 +1,17 @@
 package sorting;
 
-/**
- * Created by lip on 9/25/17.
- */
+import datastructure.BinaryHeap;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class Test {
     public static void main(String[] args) {
 
-        Integer[] unsorted = new Integer[]{6,5,4,1,3,2,0,9,8,10,7,11, 7, 7, 7};
-
-
-
-        QuickSort.sort(unsorted);
-
-
+//        Integer[] unsorted = new Integer[]{6,5,4,1,3,2,0,9,8,10,7,11, 7, 7, 7};
+        Integer[] unsorted = new Integer[]{3,2,1,4,5};
+        HeapSort.sort(unsorted);
 
         for(int i : unsorted) {
             System.out.println(i);
